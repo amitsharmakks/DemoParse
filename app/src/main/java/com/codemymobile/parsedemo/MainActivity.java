9cc09845ff434b56157a8d09105de7d2b9a4f16f
Amit Sharma <amitsharma.kks@gmail.com>
2017-06-02 18:32:05 +0530

    SignIn Feature

--- app/src/main/java/com/codemymobile/parsedemo/MainActivity.java
+++ app/src/main/java/com/codemymobile/parsedemo/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
          // getIndividualSchool();
 
           // addNewUser();
-    //  signInAccount();
+      signInAccount();
 
        // addStudent();
     //    getListOfSchool();
